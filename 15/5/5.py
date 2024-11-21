7d38e10abc0661aefc7613694d2140290e769a59
AxelThorAsp <axel.aspelund@gmail.com>
2024-11-21 19:20:40 +0000

    ...

--- 15/5/5.py
+++ 15/5/5.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import sys
+from collections import defaultdict
 
 vowels = {'a','e','i','o','u'}
 banned = {'a' : 'b', 'c' : 'd', 'p' : 'q', 'x' : 'y'}
@@ -18,10 +19,27 @@ def ok(line):
                 double = True
     return double and vcount > 2
 
+
+def ok2(line):
+    d = defaultdict(int)
+    between = False
+    last_seen = {}
+    for i, c in enumerate(line):
+        if i < len(line) - 1:
+            key = line[i:i+2]
+            if key in d and last_seen[key] == i - 1:
+                pass
+            else:
+                d[key] += 1
+                last_seen[key] = i
+        if i < len(line) - 2:
+            if line[i] == line[i + 2]:
+                between = True
+    return between and len([_ for _ in d.values() if _ > 1])
+
 t = 0
 for l in sys.stdin:
     line = l.strip()
-    if ok(line):
+    if ok2(line):
         t+=1
-
 print(t)
