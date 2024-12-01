4fb3fdd7254191ad3a8fa9d63fa2cc4af4e6b282
AxelThorAsp <axel.aspelund@gmail.com>
2024-12-01 09:15:51 +0000

    :D

--- 24/1.cpp
+++ 24/1.cpp
@@ -10,19 +10,10 @@ std::vector<int> v{};
 
 int main()
 {
-    std::string line;
     int a, b;
-    const std::regex regex(R"x((\d+)\s+(\d+))x");
-    std::smatch match;
-    while(std::getline(std::cin, line))
-    {
-        if (std::regex_search(line, match, regex))
-        {
-            a = std::stoi(match[1]);
-            b = std::stoi(match[2]);
-            v.push_back(a);
-            m[b]++;
-        }
+    while (std::cin >> a >> b) {
+        v.push_back(a);
+        m[b]++;
     }
 
     size_t t = 0;
