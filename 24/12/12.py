26dbdf850f4fff61f5c9feba025cf1ea953c11f4
AxelThorAsp <axel.aspelund@gmail.com>
2024-12-13 12:44:41 +0000

    p2

--- 24/12/12.py
+++ 24/12/12.py
@@ -7,19 +7,6 @@ H = len(D)
 W = len(D[0])
 DIRS = [(0, 1), (1, 0), (0, -1), (-1, 0)]
 
-"""
-RRRRIICCFF
-RRRRIICCCF
-VVRRRCCFFF
-VVRCCCJFFF
-VVVVCJJCFE
-VVIVCCJJEE
-VVIIICJJEE
-MIIIIIJJEE
-MIIISIJEEE
-MMMISSJEEE
-"""
-
 def in_bounds(x,y):
     return 0 <= x < W and 0 <= y < H
 
@@ -31,22 +18,37 @@ def nbors(x, y):
 class Ref:
     def __init__(self):
         self.fence = 0
+        self.sides = []
+        self.side_c = 0
 
 def walk(x, y, seen, fences):
     seen.add((x,y))
     for nbor in nbors(x,y):
         xn, yn = nbor
-        # not in bounds or not same plane
-        # therefore, fence here
         if not in_bounds(xn, yn) or D[yn][xn] != D[y][x]:
-            # maybe add check for in seen ?
+            # not in bounds or not same plant
+            # therefore, fence here
             fences.fence+=1
+            fences.sides.append(((x,y),(xn,yn)))
             continue
         if nbor not in seen:
             walk(xn, yn, seen, fences)
     return seen
+def do(l):
+    s = set()
+    for p1,p2 in l:
+        ok = True
+        x,y = p1
+        x1,y1 = p2
+        for dx,dy in [(1,0),(0,1)]:
+            if ((x+dx,y+dy),(x1+dx,y1+dy))in l:
+                ok = False
+        if ok:
+            s.add((p1,p2))
+    return len(s)
 
 area = 0
+p2 = 0
 GLOBAL_SEEN = set()
 for y, l in enumerate(D):
     for x, p in enumerate(D[y]):
@@ -54,8 +56,8 @@ for y, l in enumerate(D):
             continue
         ref = Ref()
         s = walk(x,y,set(), ref)
-        print(ref.fence)
+        p2+=len(s)*do(set(ref.sides))
         GLOBAL_SEEN.update(s)
         area+=len(s) * ref.fence
-        print(len(s))
 print(area)
+print(p2)
