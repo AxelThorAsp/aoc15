52a5c4033bd57fc5ee761b8863e8f95bc670562d
AxelThorAsp <axel.aspelund@gmail.com>
2024-12-24 00:34:19 +0000

    20

--- 24/20/20.py
+++ 24/20/20.py
@@ -3,7 +3,6 @@ from functools import cache
 import sys
 
 S = sys.stdin.read().strip()
-
 M = [list(l) for l in S.splitlines()]
 W = len(M[0])
 H = len(M)
@@ -23,11 +22,40 @@ def nbors(p):
         if 0 <= dx+x < W and 0 <= y+dy < H and M[y+dy][x+dx] != "#":
             yield (x+dx,y+dy)
 
+def nbors_all(p):
+    x,y = p
+    for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+        if 0 <= dx+x < W and 0 <= y+dy < H:
+            yield (x+dx,y+dy)
+
 def cheat(p):
     x,y = p
     for dx, dy in [(-2, 0), (2, 0), (0, -2), (0, 2)]:
         if 0 <= dx+x < W and 0 <= y+dy < H and M[y+dy][x+dx] != "#":
             yield (x+dx,y+dy)
+@cache
+def cheat2(start):
+    end = set() 
+    visited = set() 
+    d = deque([(start, 0)])
+    while d:
+        curr, dist = d.popleft()
+        x, y = curr
+        if curr in visited or dist > 20:
+            continue
+        visited.add(curr)
+        if M[y][x] == "#":
+            for nbor in nbors_all(curr):
+                if nbor not in visited:
+                    d.append((nbor, dist + 1))
+            continue
+        for nbor in nbors_all(curr):
+            if nbor not in visited:
+                d.append((nbor, dist + 1))
+        end.add((curr, dist))
+    return end
+
+
 @cache
 def bfs(s):
     d = deque([(s)])
@@ -41,13 +69,15 @@ def bfs(s):
     return distto
 
 l = bfs(START)
+lend = bfs(END)
 cheats = set()
 print(len(l))
 for i, p in enumerate(l):
-    for pp in cheat(p):
-        if pp in l and l[pp] >= l[END] - 100:
+    print(i)
+    for pp, w in cheat2(p):
+        if l[p] + 100 >= l[END]:
             continue
-        n = bfs(pp)[END]+l[p]+2
+        n = bfs(pp)[END]+l[p]+w
         if n < l[END] and (l[END] - n) >= 100:
             cheats.add((p,pp))
 print(len(cheats))
